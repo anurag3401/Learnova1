01a3e4359e410f16ebf25e3156df01a446104147
anurag3401 <anurag.navodayan2016@gmail.com>
2026-03-31 23:35:27 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config' 
+import ConnectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
+
+// Initialize Express
+const app = express()
+
+// Connect to Database
+await ConnectDB()
+
+// Middlewares
+app.use(cors())
+app.use(clerkMiddleware())
+
+// Routes
+app.get('/', (req, res)=> res.send("API Working"))
+app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter)
+
+
+
+
+// Port 
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+
+
